0593a4babc6706ec676e3e04b0658985fa769c94
Philip Yao Aidam <phlpaidam@gmail.com>
2024-08-25 19:45:40 +0000

    Add advance features and security

---
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,34 @@
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+from django.contrib import admin
+from .models import Book
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # Display the additional fields in the admin list view and user detail view
+    list_display = ('username', 'email', 'date_of_birth',
+                    'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'date_of_birth')
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .forms import ExampleForm
+
+# Create your views here.
+
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    return render(request, 'bookshelf/book_list.html')
+
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request):
+    return render(request, 'bookshelf/edit_book.html')
